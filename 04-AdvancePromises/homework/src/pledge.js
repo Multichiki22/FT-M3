0ef95a8ea21a81d32489f49f6f5a25a6293526fc
multichiki22 <myguelangel12@gmail.com>
2022-12-15 18:48:14 -0500

    Completed cap 1, 2, 3 of test

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -1,10 +1,60 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+class $Promise {
+  constructor(executor) {
+    if (typeof executor != "function") {
+      throw TypeError("/executor.+function/");
+    }
+    this._state = "pending";
+    this._handlerGroups = [];
 
+    executor(
+      (value) => {
+        this._internalResolve(value);
+      },
+      (reason) => {
+        this._internalReject(reason);
+      }
+    );
+  }
+  _internalResolve(data) {
+    if (this._state === "pending") {
+      this._state = "fulfilled";
+      this._value = data;
+      this._callHandlers();
+    }
+  }
+  _internalReject(reason) {
+    if (this._state === "pending") {
+      this._state = "rejected";
+      this._value = reason;
+      this._callHandlers();
+    }
+  }
+  _callHandlers() {
+    while (this._handlerGroups.length > 0) {
+      const group = this._handlerGroups.shift();
+      this._state === "fulfilled" &&
+        group.successCb &&
+        group.successCb(this._value);
+      this._state === "rejected" && group.errorCb && group.errorCb(this._value);
+    }
+  }
+  then(handlerSuccess, handlerError) {
+    this._handlerGroups.push({
+      successCb: typeof handlerSuccess === "function" ? handlerSuccess : false,
+      errorCb: typeof handlerError === "function" ? handlerError : false,
+    });
 
+    this._state !== "pending" && this._callHandlers();
+  }
+  catch(handlerError) {
+    this.then(null, handlerError);
+  }
+}
 
 module.exports = $Promise;
 /*-------------------------------------------------------
